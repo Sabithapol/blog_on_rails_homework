82ed078dd4654648e0a05008b7795ff7d89e5094
Sabithapol <Sabitha.poladi10@gmail.com>
2020-07-29 17:17:09 -0700

    commit four

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
 
   resources:users ,only: [:new, :create]
 
-  resource:session,only: [:new, :create, :destroy]
+  resources:session,only: [:new, :create, :destroy]
 end
